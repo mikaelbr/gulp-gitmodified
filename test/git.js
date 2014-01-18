687b55ef5b3f691ec2595d5d5edff2c5f8afcaa8
Mikael Brevik <mikaelbre@gmail.com>
2014-01-18 15:47:51 +0100

    Adds more tests for git module.

--- test/git.js
+++ test/git.js
@@ -27,6 +27,67 @@ describe("gulp-gitmodified", function () {
     });
   });
 
-  // More tests to come.
+
+  it("should give error if git is not found on the system", function (done) {
+    git.which = function (app, cb) {
+      cb(new Error());
+    };
+    git.exec = function (app, args, extra, cb) {
+      cb(null, "");
+    };
+    git.getStatusByMatcher(/\\s/, function (err) {
+      should(err.message).equal("git not found on your system.");
+      done();
+    });
+  });
+
+  it("should return a file returned by git status", function (done) {
+    git.which = function (app, cb) {
+      cb();
+    };
+    git.exec = function (app, args, extra, cb) {
+      cb(null, "M index.js");
+    };
+    git.getStatusByMatcher(/M\s/i, function (err, data) {
+      should.not.exist(err);
+      should.exist(data);
+      should.exist(data[0]);
+      data[0].should.equal('index.js');
+      done();
+    });
+  });
+
+
+  it("should return several files from git status", function (done) {
+    git.which = function (app, cb) {
+      cb();
+    };
+    git.exec = function (app, args, extra, cb) {
+      cb(null, "M index.js\nM foo.js\nM bar.js");
+    };
+    git.getStatusByMatcher(/M\s/i, function (err, data) {
+      should.not.exist(err);
+      should.exist(data);
+      should.exist(data[0]);
+      data.length.should.equal(3);
+      done();
+    });
+  });
+
+  it("should ignore statused files that doesnt match pattern", function (done) {
+    git.which = function (app, cb) {
+      cb();
+    };
+    git.exec = function (app, args, extra, cb) {
+      cb(null, "M index.js\nD foo.js\nM bar.js");
+    };
+    git.getStatusByMatcher(/M\s/i, function (err, data) {
+      should.not.exist(err);
+      should.exist(data);
+      should.exist(data[0]);
+      data.length.should.equal(2);
+      done();
+    });
+  });
 
 });
